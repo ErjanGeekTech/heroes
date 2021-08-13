9a93205f9d64058613e406257c5b1f4b30b0a586
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-08-13 13:26:46 +0300

    update

--- app/src/main/java/com/example/heroes/singleton/Singleton.kt
+++ app/src/main/java/com/example/heroes/singleton/Singleton.kt
@@ -6,9 +6,6 @@ object Singleton {
 
     fun getHaracter(): String{
         val configuration: Configuration = Configuration(0, 0, 0).Builder()
-//            .setLatency(400)
-//            .setSensitivity(9)
-//            .setUserColor(50)
             .build()
 
         return "${configuration.latency} \n ${configuration.sensitivity} \n  ${configuration.userColor}"
